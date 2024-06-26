ab0ba01dda58c27f2855de8b8ba80165827e55f8
alesilvasp <alesilvasp@gmail.com>
2024-06-26 14:38:27 -0300

    feat: adicionado getters e setters para classe Pessoa

--- src/pessoa/Pessoa.java
+++ src/pessoa/Pessoa.java
@@ -3,6 +3,23 @@ package pessoa;
 import java.time.LocalDate;
 
 public class Pessoa {
-    String nome;
-    LocalDate dataNascimento;
+    private String nome;
+    private LocalDate dataNascimento;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
 }
